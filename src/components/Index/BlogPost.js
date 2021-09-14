571525c4488f532d2e59411232cecd2c29e4a09e
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-09-15 02:56:11 +0500

    remove file

--- src/components/Index/BlogPost.js
+++ src/components/Index/BlogPost.js
@@ -1,41 +1,8 @@
 import React from "react"
 import { Link, graphql, useStaticQuery } from "gatsby"
 // abde@hotmail.com
-// import Image from "gatsby-image"
-
-// export const query = graphql`
-//   {
-//     allStrapiBlogs {
-//       nodes {
-//         title
-//         short_desc
-//         slug
-//         id
-//         date
-//         image {
-//           childImageSharp {
-//             fluid {
-//               ...GatsbyImageSharpFluid
-//             }
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
-
-// image {
-//   localFile {
-//     publicURL
-//     childImageSharp {
-//       gatsbyImageData(layout: FULL_WIDTH, placeholder: TRACED_SVG)
-//     }
-//   }
 
 const BlogPost = () => {
-  // const {
-  //   allStrapiBlogs: { nodes },
-  // } = useStaticQuery(query)
   return (
     <div id="blog" className="blog-area border-bottom ptb-100">
       <div className="common-right-text">
@@ -56,31 +23,7 @@ const BlogPost = () => {
           </p>
         </div>
 
-        <div className="row">
-          {/* { nodes.map(blog => { 
-          return (
-          <div className="col-sm-6 col-lg-6">
-            <div className="blog-item">
-              <div className="top">
-                <Link to={`/blogs/${blog.slug}`}>
-                  <Image fluid={blog.image.childImageSharp.fluid} />
-                </Link>
-                <h4>{blog.date}</h4>
-              </div>
-              <div className="bottom">
-                <h3>
-                  <Link to={`/blogs/${blog.slug}`}>{blog.title}</Link>
-                </h3>
-                <p>{blog.short_desc}</p>
-
-                <Link to={`/blogs/${blog.slug}`} className="blog-btn">
-                  Read More <i className="flaticon-right-arrow"></i>
-                </Link>
-              </div>
-            </div>
-          </div>
-          ))}  */}
-        </div>
+        <div className="row"></div>
 
         <div className="text-center">
           <Link to="/blog" className="common-btn">
