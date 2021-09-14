485e4bf7531a3c94af53bc93e1154d741b6ac83f
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2021-09-15 02:50:39 +0500

    add config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,6 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
 module.exports = {
   siteMetadata: {
     title: `my own Blog`,
@@ -20,7 +23,7 @@ module.exports = {
     {
       resolve: `gatsby-source-strapi`,
       options: {
-        apiURL: "http://localhost:1337",
+        apiURL: process.env.API_URL || "http://localhost:1337",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "articles",
